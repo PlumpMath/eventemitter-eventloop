52b18d60325ffc7925d1c29ffd3d65a3667de7ec
Matthew Schnee <matthew.schnee@gmail.com>
2016-01-01 19:46:16 -0800

    Uses googletest for unit testing :3

--- test/generic_test.cpp
+++ test/generic_test.cpp
@@ -21,88 +21,149 @@ SOFTWARE.
 **/
 #include "Emitter.h"
 #include "Registry.h"
-#include "Log.h"
+#include <gtest/gtest.h>
 #include <thread>
 #include <chrono>
 #include <mutex>
 #include <iostream>
+#include <chrono>
+typedef std::chrono::high_resolution_clock Clock;
 
+#include <gtest/gtest.h>
 
-namespace {
-	std::mutex g_mutex;
-	bool g_running = true;
-	EventEmitter::Emitter* g_emitter = nullptr;
-	auto testEvent = EventEmitter::EventId(1);
-	EventEmitter::EventId rvalueEvent = 2u;
-} // anonymous namespace
+EventEmitter::EventId s_testEvent = 1;
 
-
-class TestBase
+TEST(Sanity, Defaults)
 {
-public:
-	explicit TestBase(std::function<void()> callFunction)
-		: m_function(callFunction)
-	{}
+	EXPECT_TRUE(true);
+	EXPECT_EQ(2, 2);
+}
 
-	void run()
+/**
+ *	Test that Emitter->On(eventId, callback, EventEmitter::Immediate) is called immediately on the main thread.
+ */
+TEST(On, On_Immediate_Is_Called_On_Same_Thread)
+{
+	auto e = std::unique_ptr<EventEmitter::Emitter>(new EventEmitter::Emitter);
+	auto currentThreadId = std::this_thread::get_id();
+
+	volatile bool testWasRun = false;
+
+	e->On(s_testEvent, [&currentThreadId, &testWasRun]()
 	{
-		using namespace std::literals;
+		EXPECT_EQ(currentThreadId, std::this_thread::get_id());
+		testWasRun = true;
+	}, EventEmitter::Immediate);
+	auto testStart = Clock::now();
+	e->Emit(s_testEvent);
 
-		// scope to serialize access to std::cout
-		LOG("             binding event on thread " << std::this_thread::get_id())
+	while (!testWasRun || std::chrono::duration_cast<std::chrono::nanoseconds>(Clock::now() - testStart).count() < 1000)
+	{
+		// wait for the test to run.
+	}
+	EXPECT_TRUE(testWasRun);
+}
 
-			g_emitter->On(testEvent, m_function, EventEmitter::Dispatch);
-		do
+/**
+ *	Test that Emitter->On(eventId, callback, EventEmitter::Async) is not called on the main thread.
+ */
+TEST(On, On_Async_Is_Not_Called_On_Main_Thread)
+{
+	auto e = std::unique_ptr<EventEmitter::Emitter>(new EventEmitter::Emitter);
+	auto currentThreadId = std::this_thread::get_id();
+	volatile bool testWasRun = false;
+
+	e->On(s_testEvent, [&currentThreadId, &testWasRun]()
+	{
+		EXPECT_NE(currentThreadId, std::this_thread::get_id());
+		testWasRun = true;
+	}, EventEmitter::Async);
+
+	auto testStart = Clock::now();
+	e->Emit(s_testEvent);
+	while (!testWasRun || std::chrono::duration_cast<std::chrono::nanoseconds>(Clock::now() - testStart).count() < 1000)
+	{
+		// wait for the test to run.
+	}
+	EXPECT_TRUE(testWasRun);
+}
+
+/**
+ *	Test that Emitter->On(eventId, callback) is called as if EventEmitter::Async was passed.
+ */
+TEST(On, On_Default_Behavior_Is_Async)
+{
+	auto e = std::unique_ptr<EventEmitter::Emitter>(new EventEmitter::Emitter);
+	auto currentThreadId = std::this_thread::get_id();
+	volatile bool testWasRun = false;
+
+	e->On(s_testEvent, [&currentThreadId, &testWasRun]()
+	{
+		EXPECT_NE(currentThreadId, std::this_thread::get_id());
+		testWasRun = true;
+	});
+
+	auto testStart = Clock::now();
+	e->Emit(s_testEvent);
+	while (!testWasRun || std::chrono::duration_cast<std::chrono::nanoseconds>(Clock::now() - testStart).count() < 1000)
+	{
+		// wait for the test to run.
+	}
+	EXPECT_TRUE(testWasRun);
+}
+
+/**
+ *	Test that Emitter->On(eventId, callback, EventEmitter::Dispatch) is called on the correct thread.
+ *	This requires setting up a thread which will call On.  For simplicity, we don't use a two-lock scheme for
+ *	synchronization.  Intead, we give the thread 2 seconds to start, assume it attaches a callback, and then Emit().
+ *
+ *	TODO: Assumptions are bad in unit tests.  This should be synchronized.
+ */
+TEST(On, On_Dispatch_Is_Called_On_Dispatching_Thread)
+{
+	auto e = std::unique_ptr<EventEmitter::Emitter>(new EventEmitter::Emitter);
+	auto eptr = e.get();
+	volatile bool testWasRun = false;
+	auto currentThreadId = std::this_thread::get_id();
+
+	std::thread t([&currentThreadId, &testWasRun, eptr]()
+	{
+		auto threadId = std::this_thread::get_id();
+		volatile bool wait = true;
+		eptr->On(s_testEvent, [&threadId, &currentThreadId, &wait, &testWasRun]()
 		{
-			std::this_thread::sleep_for(1s);
+			EXPECT_EQ(threadId, std::this_thread::get_id());
+			EXPECT_NE(currentThreadId, threadId);
+			EXPECT_NE(currentThreadId, std::this_thread::get_id());
+			wait = false;
+			testWasRun = true;
+		}, EventEmitter::Dispatch);
+
+		while (wait)
+		{
 			EventEmitter::Registry::ProcessEvents();
-		} while (g_running);
-	}
+		}
+	});
+	t.detach();
 
-	std::function<void()> m_function;
-};
-
-int main(int argc, char** argv)
-{
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+	auto testStart = Clock::now();
+	e->Emit(s_testEvent);
+	while (!testWasRun)
 	{
-		using namespace std::literals;
-
-		g_emitter = new EventEmitter::Emitter;
-
-		g_emitter->On(testEvent, []()
+		auto current = Clock::now();
+		auto d = std::chrono::duration_cast<std::chrono::seconds>(current - testStart).count();
+		if (d > 3)
 		{
-			LOG("[EVENT      ] This should be the main thread " << std::this_thread::get_id())
-		}, EventEmitter::Immediate);
-
-
-		g_emitter->On(testEvent, []()
-		{
-			LOG("[EVENT ASYNC] This should be async " << std::this_thread::get_id())
-		});
-
-
-		auto t1 = TestBase([]()
-		{
-			LOG("[EVENT t1   ] This should be the first thread " << std::this_thread::get_id())
-		});
-
-		auto t2 = TestBase([]()
-		{
-			LOG("[EVENT t2   ] This should be the second thread " << std::this_thread::get_id())
-		});
-
-
-		std::thread thread1(&TestBase::run, t1);
-		std::thread thread2(&TestBase::run, t2);
-		thread1.detach();
-		thread2.detach();
-
-		std::this_thread::sleep_for(4s);
-		LOG("=============== FIRING EVENT 1 FROM " << std::this_thread::get_id() << " ==================")
-			g_emitter->Emit(testEvent);
-		std::this_thread::sleep_for(4s);
-		g_running = false;
-
+			break;
+		}
+		// wait for the test to run.
 	}
-	system("pause");
+	EXPECT_TRUE(testWasRun);
+}
+
+int main(int argc, char* argv[])
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
